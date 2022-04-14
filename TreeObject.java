40d8dd1f3d60e9f45deca56c3305e76c59d1dbe1
GaryBrusse <garybrusse@onyxnode68.boisestate.edu>
2022-04-14 13:29:31 -0600

    first push

---
+++ TreeObject.java
@@ -0,0 +1,20 @@
+public class TreeObject {
+    long DNA = -1;
+    int frequency = 0;
+
+    public TreeObject(long DNA) {
+        this.DNA = DNA;
+        
+    }
+    public long getDNA() {
+        return DNA;
+    }
+    public void increase_frequency(){
+        //How many times a given DNA sequence occurs
+        frequency++;
+    }
+    public String counter(int numChar){
+        //numChar: the number of chars given in a DNA sequence.
+        return null;
+    }
+}
