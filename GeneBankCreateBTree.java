f32ee9c7ad3ae4b06cccd47d9d77ca9ccc5a508a
Tyler-Bowes <97471311+Tyler-Bowes@users.noreply.github.com>
2022-04-26 13:55:47 -0600

    Update GeneBankCreateBTree.java
    minor changes to file

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -3,83 +3,94 @@ import java.io.FileNotFoundException;
 
 public class GeneBankCreateBTree {
 
-	public GeneBankCreateBTree() {
-		// TODO Auto-generated constructor stub
-	}
+	
 
 	public static void main(String[] args) {
+		
+		BTree btree;
+		int cacheSize; //will change if we want a cache
+		int degree;
+		int sequenceLength;
+		
 		int argsLength = args.length; //doing this so i dont have to call args.length a bunch
 		try {
-			if(argsLength >= 4 && argsLength <= 6) {
-				//first check the first 4 maditory arguments
-				//NOTE: if with cache args[4] is also maditory
+			if(argsLength >= 4 && argsLength <= 6) {//first check the first 4 maditory arguments
 				
-				if(args[0].equals("1")) { // checks either 1 or 0
+				if(args[0].equals("1")) { //checks for cache NOTE: if with cache args[4] is also maditory
 					
-					if(argsLength == 5) {//implement cache by checking first if we have a size for it
-						
-						int cacheSize = Integer.parseInt(args[4]);
+					if(argsLength == 5) {//since we want cache check size for cache
+						cacheSize = Integer.parseInt(args[4]);
 						if(cacheSize <= 0) { // we will test sizes 100 and 500
 							throw new Exception("ERROR: Invalid cache size value"); //throw an error to stop program b/c its invalid
 						
 						}else {
-							//valid cache size so implemet cache
-							//NOTE we will want to store the cache after we finish to use as a parameter for search algo
+							//implementing btree later covers making the cache with cacheSize
 						}
 					}else { //there is no arguement for the size
-						throw new Exception("ERROR: No cache size value"); //throw an error to stop program b/c its invalid
+						throw new Exception("ERROR: No cache size value");
 					}
 					
 				} else if(args[0].equals("0")) { //do not implement cache
-
+					cacheSize = 0; //this will not create a cache when creating the btree
+					
 				} else { //args[0] is not 1 or 0
 					throw new Exception("ERROR: Invalid cache value"); //throw an error to stop program basically
 				}
 				
 				//degree
-				int degree = Integer.parseInt(args[1]);
+				degree = Integer.parseInt(args[1]);
 				if(degree >= 0 && degree < 4096) { 
 					if(degree == 0) {
+						
 						// code for chooseing optimum degree
+					
 					}
-					System.out.println(degree);	
+					//System.out.println(degree);	
 					
 				} else { //the degree is not valid
 					throw new Exception("ERROR: Invalid degree");//throw an error to stop program
 				}
 				
-				//gbk file
-				File file = new File(args[2]);
-				if(file.exists() && file.isFile()) { //file exits
-					System.out.println("valid file");			
-
-				} else { //file is not valid
-					throw new FileNotFoundException();//throw an error to stop program
-				}
-				
 				
 				//sequence length
-				int sequenceLength = Integer.parseInt(args[3]);
-				if(sequenceLength > 0 && sequenceLength < 32) { // 1 to 31 (inclusive)
-					System.out.println(sequenceLength);			
-				} else {
-					//throw error maybe?
+				sequenceLength = Integer.parseInt(args[3]);
+				if(sequenceLength <= 1 && sequenceLength > 31) { // length is over 31 
 					throw new Exception("ERROR: Invalid sequenceLenth size");//throw an error to stop program
-				}
+				} 
 			
 				//check for the last argument
-				if(argsLength == 6) { // has 6 arguments (which is now optional) so make sure it is valid
-					if(args[5].equals("1")) { //creates a dump file
+				if(argsLength == 6) { // check arg 6 (which is optional) then make sure it is valid
+					if(args[5].equals("1")) { 
 						
-					}else if(args[5].equals("0")) { //no dump file just print in console
+						//creates a dump file
+						
+					}else if(args[5].equals("0")) { 
+						
+						//no dump file (print in console?)
 						
 					}else {
 						throw new Exception("ERROR: Invalid debug level"); //throw an error to stop program
 						//error not valid debug level
 					}
 				}
-			}else {
-				throw new Exception("ERROR: Invalid usage"); //throw an error to stop program 
+				
+				//gbk file last because this is where we create our BTree
+				File file = new File(args[2]);
+				if(file.exists() && file.isFile()) {
+					System.out.println("Degree: " + degree + "\nCacheSize: " + cacheSize + "\nsequenceLength: " + sequenceLength);
+					System.out.println("File: " + args[2]);
+					//btree = new BTree(degree, cacheSize, sequenceLength, "test");
+					//readFile(file, sequenceLength, btree);
+					
+					
+					
+				} else { //file is not valid
+					throw new FileNotFoundException();//throw an error to stop program
+				}
+				
+				
+			}else { //invald # args
+				throw new Exception("ERROR: Invalid usage"); //args > 6 and args < 4
 			}
 		}catch(FileNotFoundException e){
 			e.printStackTrace();
