545f67fb19b52c06d94b9c65acc74590a3b5045f
Tyler-Bowes <97471311+Tyler-Bowes@users.noreply.github.com>
2022-05-03 16:17:39 -0600

    added another tostring method for char sequence

--- TreeObject.java
+++ TreeObject.java
@@ -15,4 +15,33 @@ public class TreeObject {
     public String toString() {
         return String.valueOf(sequence);
     }
+	
+    public String toString(int sequenceLength) {
+    	long testseq = sequence;
+    	testseq = sequence >> 1;
+    	String stringSeq = "";
+    	byte numcheck = 0;
+    		for(int i = 2*sequenceLength - 1; i>=0; i -= 2) {
+    			numcheck = 0;
+    			numcheck |= (testseq >> (i-1));
+    			switch(numcheck) {
+    		    case 0:
+    		    	stringSeq = stringSeq.concat("a");
+    		    	break;
+    		    case 1:
+    		    	stringSeq = stringSeq.concat("c");
+    		    	break;
+    		    case 2:
+    		    	stringSeq = stringSeq.concat("g");
+    		    	break;
+    		    case 3:
+    		    	stringSeq = stringSeq.concat("t");
+    		    	break;
+    		   }
+    			testseq &= ~(1<<i);
+    			testseq &= ~(1<<i-1);
+    		}
+    				
+    	return stringSeq;
+    }
 }
